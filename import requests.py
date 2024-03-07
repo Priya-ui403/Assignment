9d747b5da0a9814789a6133dab355ca5fdeebfe7
Priya <112187361+Priya-ui403@users.noreply.github.com>
2024-03-07 21:50:30 +0530

    Update import requests.py

--- import requests.py
+++ import requests.py
@@ -1,30 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
-from flask import Flask, jsonify
+import json
+from flask import Flask
+
 
 app = Flask(__name__)
 
-@app.route('/getTimeStories', methods=['GET'])
-def get_time_stories():
-    # Fetch the HTML content from the specified URL
-    url = 'https://time.com'
+
+def get_latest_stories():
+    url = "https://time.com"
+    
+    # Send a GET request to the URL
     response = requests.get(url)
-    html_content = response.text
+    
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the HTML content of the page using BeautifulSoup
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        # Find the elements containing the latest stories
+        story_elements = soup.find_all('div', class_='partial latest-stories')
+        
+        # Extract and print the titles of the latest stories
+        #print(story_elements)
+        titles_array = []
+        links_array = []
+        for story_element in story_elements:
+            titles = story_element.find_all('h3', class_='latest-stories__item-headline')
+            for title in titles:
+                titles_array.append(title.text)
+                #print(title.text)
 
-    # Parse HTML content using BeautifulSoup
-    soup = BeautifulSoup(html_content, 'html.parser')
+            for a in story_element.find_all('a', href=True):
+              links_array.append("https://time.com" + a['href'])
+              #print(a['href'])
 
-    # Find the latest 6 stories based on the HTML structure
-    latest_stories = []
-    stories_elements = soup.select('.latest-stories .swipe-container .card')
+            score_titles = [{"title": t, "link": s} for t, s in zip(titles_array, links_array)]
+            return json.dumps(score_titles)
 
-    for story_element in stories_elements[:6]:
-        title = story_element.select_one('.headline').get_text(strip=True)
-        link = story_element.select_one('a')['href']
-        latest_stories.append({'title': title, 'link': link})
 
-    # Return the latest 6 stories as a JSON response
-    return jsonify(latest_stories)
+                
+    else:
+        print(f"Failed to retrieve the page. Status code: {response.status_code}")
+
+@app.route('/getTimeStories', methods=['GET'])
+def get_Time_Stories():   
+    return get_latest_stories()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
